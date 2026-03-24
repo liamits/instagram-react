5c03a4064f0b389d50fb664121d46a6aaaabd317
Dinh Quang Cuong <dqcuong.work@gmail.com>
2026-03-24 13:54:30 +0700

    fix: move auth require before usage in userRoutes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getUserProfile, followUnfollowUser, searchUsers, updateProfile, getUserById, getSuggestions } = require('../controllers/userController');
-
-router.get('/suggestions', auth, getSuggestions);
 const auth = require('../middleware/authMiddleware');
 
+router.get('/suggestions', auth, getSuggestions);
 router.get('/search', searchUsers);
 router.get('/profile/:username', getUserProfile);
 router.get('/profile/id/:id', getUserById);
